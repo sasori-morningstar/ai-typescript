858f22a75ab91af8be0ad1e7b7a674ec12775891
Sasori Morningstar <ilyes.medjedoub@etu.usthb.dz>
2025-03-31 19:07:50 +0100

    Created a CLI to facilitate tests

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,34 @@
-//import * as tf from '@tensorflow/tfjs-node'
-
-/*const tensor1 = tf.tensor([1, 2, 3, 4, [2, 2]])
-const tensor2 = tf.tensor([1, 2, 3, 4], [2, 2])
-tf.add(tensor1, tensor2).print()*/
-
-/*import { trainModel } from "./linear.ts";
-
-trainModel()*/
-
+import * as tf from '@tensorflow/tfjs-node'
+import * as readlineSync from "readline-sync";
+import { trainModel } from "./linear.ts";
 import { classifyImage } from "./mobilenet.ts";
 
-classifyImage()
+
+
+const main = async () => {
+    process.stdout.write("\x1Bc");
+
+    console.log('Tensorflow TS Testing\n')
+    while (true) {
+        const userInput = readlineSync.question("What do you want to test (type 'help' for options or 'exit' to quit): ").trim().toLowerCase();
+
+        if (userInput === "exit") {
+            console.log("\nGood bye!")
+            break;
+        } else if (userInput === "linear") {
+            trainModel()
+        } else if (userInput === "image") {
+            classifyImage()
+        } else if (userInput === "help") {
+            console.log("\nAvailable commands: linear, image, exit")
+        } else {
+            console.log("\nInvalid input. Please try again. Type 'help' for options.");
+        }
+    }
+
+
+
+}
+
+main();
+
